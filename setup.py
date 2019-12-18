7dbfa7e7fe598803fbbbaba56bff700fff9cc46c
= <=>
2019-12-18 12:41:49 +0200

    First Commit

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-setup(ext_modules=cythonize('hdial_libc.pyx'), include_dirs=[np.get_include()], requires=['cv2'])
+setup(ext_modules=cythonize('hdial_libc.pyx'),
+      include_dirs=[np.get_include()],
+      requires=['cv2', 'cython', 'numba', 'numpy']
+      )
